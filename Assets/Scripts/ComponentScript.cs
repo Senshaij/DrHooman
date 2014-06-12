d66841fd891a0d4c1d6cfa07b34a525025760e12
Senshaij <senshaij@gmail.com>
2014-06-12 07:47:33 -0400

    XP Components and Simple Leveling
    Added the Love Component.
    Added XP, XP gain, and Leveling.
    Player picks up a component by walking over it.

---
+++ Assets/Scripts/ComponentScript.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComponentScript : MonoBehaviour {
+	//amount of xp the component grants the player
+	public int xpGrant = 1;
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//player controller and behaviours
+
+public class PlayerScript : MonoBehaviour {
+
+	//player speed
+	public Vector2 speed = new Vector2(20,20);
+	//where player speed is stored
+	private Vector2 movement;
+	//player's XP and level
+	//NOTE: public for testing purposes only
+	public int xp = 0;
+	public int level = 1;
+	public int levelFactor = 10;
+
+	// Update is called once per frame
+	void Update () {
+		//Horizontal and Vertical are defined in Unity w/
+		//specific key bindings
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		//movement in each direction
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate () {
+		//move the game object
+		rigidbody2D.velocity = movement;
+	}
+
+	public void GainXP (int xpGained) {
+		xp += xpGained;
+
+		if (xp >= (level * levelFactor)) {
+			level += 1;
+			xp = 0;
+		}
+	}
+
+	//did player pick up a component?
+	void OnTriggerEnter2D (Collider2D collider) {
+		ComponentScript comp = collider.gameObject.GetComponent<ComponentScript> ();
+		
+		if (comp != null) {
+			GainXP (comp.xpGrant);
+			Destroy (comp.gameObject);
+		}
+	}
+}
