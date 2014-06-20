8ccbcc0446416e04ea882fc4477564f4f1b7c9fb
Senshaij <senshaij@gmail.com>
2014-06-20 07:52:29 -0400

    2D Movement Rework
    Changing from top-down to flat platformer view. Working on making the
    player jump. Still needs work.
    
    Added new player texture and added some platforms.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,9 +6,11 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
 	//player speed
-	public Vector2 speed = new Vector2(20,20);
+	public float speed = 20.0F;
+	public float jumpSpeed = 10.0F;
+	public float gravity = 20.0F;
 	//where player speed is stored
-	private Vector2 movement;
+	private Vector2 movement = Vector2.zero;
 	//player's XP and level
 	//NOTE: public for testing purposes only
 	public int xp = 0;
@@ -17,13 +19,39 @@ public class PlayerScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//Horizontal and Vertical are defined in Unity w/
-		//specific key bindings
-		float inputX = Input.GetAxis ("Horizontal");
-		float inputY = Input.GetAxis ("Vertical");
+		/*
+		 * 3D-based movement
+		CharacterController controller = GetComponent<CharacterController> ();
 
+		if (controller.isGrounded) {
+			movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			movement = transform.TransformDirection(movement);
+			movement *= speed;
+
+			if (Input.GetButton("Jump")) {
+				movement.y = jumpSpeed;
+			}
+		}
+
+		movement.y -= gravity * Time.deltaTime;
+		controller.Move (movement * Time.deltaTime);
+		*/
+
+
+		//Horizontal and Jump are defined in Unity w/
+		//specific key bindings
+		movement = new Vector2 (Input.GetAxis ("Horizontal"), 0);
+		movement *= speed;
+
+		if (rigidbody2D.velocity.y == 0.0) {
+			if (Input.GetButton ("Jump")) {
+				movement.y = jumpSpeed;
+			}
+		}
+
+		//movement.y -= gravity * Time.deltaTime;
 		//movement in each direction
-		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+		//movement = new Vector2 (speed.x * inputX, speed.y * inputY);
 	}
 
 	void FixedUpdate () {
