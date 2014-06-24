81288c6d77185dcedf28c938b2fcea24143a68a5
Senshaij <senshaij@gmail.com>
2014-06-24 17:05:49 -0400

    Jumping is a go!
    Finished adding 2D jumping with gravity.
    
    TODO:
    - Level design for level/stage 1
    - UI during level/stage gameplay
    - Menu/Main page

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,16 +6,23 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
 	//player speed
-	public float speed = 20.0F;
-	public float jumpSpeed = 10.0F;
-	public float gravity = 20.0F;
-	//where player speed is stored
-	private Vector2 movement = Vector2.zero;
+	public float maxSpeed = 20.0F;
+	public float moveForce = 200.0F;
+	public float jumpForce = 1000.0F;
+	private bool grounded = false;
+	private bool jump = false;
+	private Transform groundCheck;
 	//player's XP and level
 	//NOTE: public for testing purposes only
 	public int xp = 0;
 	public int level = 1;
 	public int levelFactor = 10;
+
+	void Awake() {
+		//set up references
+		groundCheck = transform.Find ("groundCheck");
+		//TODO: animator will go here later
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -37,26 +44,31 @@ public class PlayerScript : MonoBehaviour {
 		controller.Move (movement * Time.deltaTime);
 		*/
 
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 
-		//Horizontal and Jump are defined in Unity w/
-		//specific key bindings
-		movement = new Vector2 (Input.GetAxis ("Horizontal"), 0);
-		movement *= speed;
-
-		if (rigidbody2D.velocity.y == 0.0) {
-			if (Input.GetButton ("Jump")) {
-				movement.y = jumpSpeed;
-			}
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			jump = true;
 		}
-
-		//movement.y -= gravity * Time.deltaTime;
-		//movement in each direction
-		//movement = new Vector2 (speed.x * inputX, speed.y * inputY);
 	}
 
 	void FixedUpdate () {
-		//move the game object
-		rigidbody2D.velocity = movement;
+		float h = Input.GetAxis ("Horizontal");
+
+		if (h * rigidbody2D.velocity.x < maxSpeed) {
+			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		}
+
+		if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed) {
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+		//TODO: add flipping to player to face the right direction
+
+		if (jump) {
+			//add a vertical force
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
 	}
 
 	public void GainXP (int xpGained) {
